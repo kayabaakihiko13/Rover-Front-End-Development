84c2f20011d3f0bf8e6c8d4f50c61009c9727074
kayabaakihiko13 <iqbalramad75@gmail.com>
2025-11-17 23:19:23 +0700

    feat: fixing calling Components for FooterSection

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <script setup>
 import Header from "./components/HeaderSection.vue";
-// import Footer from "./components/FooterSection.vue"
+import Footer from "./components/FooterSection.vue"
 </script>
 
 <template>
@@ -14,9 +14,6 @@ import Header from "./components/HeaderSection.vue";
        <RouterView />
      </main>
     <!-- Footer Header Section -->
-    <footer class="py-6 text-center text-gray-500 text-sm border-t">
-      Hak Cipta © 2025 <span class="font-semibold text-green-700">PalmDetector</span>
-    </footer>
   </div>
 
 </template>
