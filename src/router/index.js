d6093b7e646113863a9a6ae367f9d1f646ee80c6
kayabaakihiko13 <iqbalramad75@gmail.com>
2025-11-18 13:12:21 +0700

    feat: adding main router

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,10 @@
 // src/router/index.js
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-
+import RegisterView from '../views/RegisterView.vue'
 const routes = [
     { path: '/', component: HomeView },
-
+    { path: '/register', component: RegisterView },
 ]
 
 export default createRouter({
